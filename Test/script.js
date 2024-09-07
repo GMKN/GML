83981b2f15fbcb787a411396ea6da5c69d39c5c9
GMKN <gmohanraj.kingston@gmail.com>
2024-09-07 07:45:57 +0530

    Delete Test directory

--- Test/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('This form submission is not supported in this static version.');
-});
